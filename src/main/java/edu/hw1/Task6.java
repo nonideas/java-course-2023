a2157227b94fc5b46814761fc1079d520179e7c9
Alexey Oborin <a.oborin@g.nsu.ru>
2023-10-10 14:19:17 +0700

    Task 6

---
+++ src/main/java/edu/hw1/Task6.java
@@ -0,0 +1,40 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Arrays;
+
+public class Task6 {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private Task6() {
+    }
+
+    public static int countK(int number) {
+        if (number < 1000 || number > 9999) {
+            throw new IllegalArgumentException("Введено некорректное четырехзначное число.");
+        }
+        if (number == 6174) {
+            return 0;
+        }
+
+        char[] digits = String.valueOf(number).toCharArray();
+        Arrays.sort(digits);
+
+        int ascendingNumber = Integer.parseInt(new String(digits));
+
+        StringBuilder reversed = new StringBuilder(new String(digits)).reverse();
+        int descendingNumber = Integer.parseInt(reversed.toString());
+
+        int difference = descendingNumber - ascendingNumber;
+
+        return 1 + countK(difference);
+    }
+
+    public static void main(String[] args) {
+        LOGGER.info(countK(3524));
+        LOGGER.info(countK(6621));
+        LOGGER.info(countK(6554));
+        LOGGER.info(countK(1234));
+    }
+}
