6fba2cfb0fbed93c0dd6190d48c2e90c4c481f1e
Alexey Oborin <a.oborin@g.nsu.ru>
2023-10-10 21:21:41 +0700

    Task 8

---
+++ src/main/java/edu/hw1/Task8.java
@@ -0,0 +1,72 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Task8 {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private Task8() {
+    }
+
+    public static boolean knightBoardCapture(int[][] board) {
+        int[][] moves = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+        int size = board.length;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == 0) {
+                    continue;
+                }
+                for (int[] move : moves) {
+                    int checkRow = i + move[0];
+                    int checkCol = j + move[1];
+                    if (checkRow >= 0 && checkRow < size && checkCol >= 0 && checkCol < size &&
+                        board[checkRow][checkCol] == 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] board1 = {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+        LOGGER.info(knightBoardCapture(board1));
+
+        int[][] board2 = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        LOGGER.info(knightBoardCapture(board2));
+
+        int[][] board3 = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        LOGGER.info(knightBoardCapture(board3));
+    }
+}
---
+++ src/test/java/edu/hw1/KnightBoardCaptureTest.java
@@ -0,0 +1,111 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class KnightBoardCaptureTest {
+    @Test
+    @DisplayName("Ни один конь не может захватить другого коня")
+    void testKnightBoardCapture1() {
+        int[][] board = {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+        assertTrue(Task8.knightBoardCapture(board));
+    }
+
+    @Test
+    @DisplayName("Конь может захватить другого коня")
+    void testKnightBoardCapture2() {
+        int[][] board = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        assertFalse(Task8.knightBoardCapture(board));
+    }
+
+    @Test
+    @DisplayName("Конь может захватить другого коня 2")
+    void testKnightBoardCapture3() {
+        int[][] board = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assertFalse(Task8.knightBoardCapture(board));
+    }
+
+    @Test
+    @DisplayName("Передаю null")
+    void testKnightBoardCaptureEmptyBoard() {
+        int[][] board = new int[8][8];
+        assertTrue(Task8.knightBoardCapture(board));
+    }
+
+    @Test
+    @DisplayName("Один конь")
+    void testKnightBoardCaptureSingleKnight() {
+        int[][] board = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assertTrue(Task8.knightBoardCapture(board));
+    }
+
+    @Test
+    @DisplayName("Пустая доска")
+    void testKnightBoardCaptureNonKnight() {
+        int[][] board = {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assertTrue(Task8.knightBoardCapture(board));
+    }
+
+    @Test
+    @DisplayName("Вся доска в конях")
+    void testKnightBoardCaptureAllKnights() {
+        int[][] board = {
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1}
+        };
+        assertFalse(Task8.knightBoardCapture(board));
+    }
+}
